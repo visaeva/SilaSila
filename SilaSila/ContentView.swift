6d1c7f9bce729a0bede63463b31cbccba01bcf8e
visaeva <visaeva0302@gmail.com>
2024-05-23 11:49:49 +0300

    Initial Commit

---
+++ SilaSila/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SilaSila
+//
+//  Created by Victoria Isaeva on 23.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SilaSila/SilaSilaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilaSilaApp.swift
+//  SilaSila
+//
+//  Created by Victoria Isaeva on 23.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SilaSilaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
